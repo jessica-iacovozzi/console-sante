6679c2f0c72733f742b2a6e9d977bc497619cb15
Jessica Iacovozzi <iacovozzi.jessica@gmail.com>
2024-04-23 15:48:02 -0400

    Created generic template view for home page with logo

--- console_santé/urls.py
+++ console_santé/urls.py
@@ -1,14 +1,16 @@
 import debug_toolbar
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
 
 admin.site.site_header = 'Console santé admin'
 admin.site.index_title = 'Administration'
 admin.site.site_title = 'Console Santé'
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('soins/', include('unité_de_soins.urls')),
     path('auth/', include('djoser.urls')),
