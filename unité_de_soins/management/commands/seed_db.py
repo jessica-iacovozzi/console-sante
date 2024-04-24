5ec6e9df49995d30840e52a2e53628832a99cb6c
Jessica Iacovozzi <iacovozzi.jessica@gmail.com>
2024-04-24 12:57:53 -0400

    Created seperate seed for prod db

--- unité_de_soins/management/commands/seed_db.py
+++ unité_de_soins/management/commands/seed_db.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.db import connection
+from django.conf import settings
 from pathlib import Path
 import os
 
@@ -10,7 +11,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print('Populating the database...')
         current_dir = os.path.dirname(__file__)
-        file_path = os.path.join(current_dir, 'seed.sql')
+        file_path = os.path.join(current_dir, 'dev_seed.sql' if settings.DEBUG else 'prod_seed.sql')
         sql = Path(file_path).read_text()
 
         with connection.cursor() as cursor:
