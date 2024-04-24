d99528cdca126e7d4af5faf62441643c0a1af84c
Jessica Iacovozzi <iacovozzi.jessica@gmail.com>
2024-04-24 10:16:10 -0400

    Created Procfile + installed gunicorn and dj_database_url

--- console_santé/settings/prod.py
+++ console_santé/settings/prod.py
@@ -1,8 +1,13 @@
 import os
 from .common import *
+import dj_database_url
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['consolesante-prod-d3300390ee21.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
